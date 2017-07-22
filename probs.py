2b5b3c6cb77b77c16cf374639bad1040d43f462c
acflorea <acflorea@gmail.com>
2017-07-22 13:16:18 +0300

    add legend

--- probs.py
+++ probs.py
@@ -28,11 +28,11 @@ for i in x:
                                           (1 - y1[i - 1]) * (np.float(i) / (N * (ts - 1)) * (
                                               float(ts - 1 - i + np.log(ts - 1) - np.log(i)))))
 
-plt.plot(x, y1, 'r')
-plt.plot(x, yt, 'r--')
-plt.plot(x, z1, 'g')
-plt.plot(x, zt, 'g--')
-# plt.plot(x, 0.5 * y1 + 0.5 * z1, 'b')
-# plt.plot(x, 0.5 * yt + 0.5 * zt, 'b--')
+y1p, = plt.plot(x, y1, 'r', label="k=0 - Find opt and stop early")
+y1pt, = plt.plot(x, yt, 'r--', label="k=0 - Find opt regardless of early stopping")
+z1p, = plt.plot(x, z1, 'b', label="k=1 - Find opt and stop early")
+z1pt, = plt.plot(x, zt, 'b--', label="k=1 - Find opt regardless of early stopping")
+
+plt.legend(handles=[y1p, y1pt, z1p, z1pt])
 
 plt.show()
